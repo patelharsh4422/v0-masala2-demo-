cb45cad8484da43e38fea85774b91d1d0eaca40c
patelharsh4422 <patelharsh17308@gmail.com>
2026-03-19 18:49:48 +0530

    Change Supabase API key in createClient function
    Updated Supabase client with a new API key.

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,6 +2,6 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     'https://seaqcywibeqovxhlhovv.supabase.co', 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlYXFjeXdpYmVxb3Z4aGxob3Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTc5MjQsImV4cCI6MjA4OTM3MzkyNH0.cuT3blmyljExjVIJ6bTyIpeDnTQSM9VUd3XT735mRYI'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlYXFjeXdpYmVxb3Z4aGxob3Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTc5MjQsImV4cCI6MjA4OTM3MzkyNH0.cuT3blmyljExjVIJ6bTyIpeDnTQSM9VUd3XT735mRYI',
   )
 }
