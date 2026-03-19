e6c5101507921c8f39f980646b3fa8527b3c4c5c
patelharsh4422 <patelharsh17308@gmail.com>
2026-03-19 18:41:26 +0530

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,6 @@
-import { createBrowserClient } from '@supabase/ssr'
-
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    'https://seaqcywibeqovxhlhovv.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlYXFjeXdpYmVxb3Z4aGxob3Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTc5MjQsImV4cCI6MjA4OTM3MzkyNH0.cuT3blmyljExjVIJ6bTyIpeDnTQSM9VUd3XT735mRYI'
   )
 }
